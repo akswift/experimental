00c704af2ba8e15c70ce3fb1c9d1db40c5c8a9ef
akswift <akswift.freelance@gmail.com>
2020-01-18 18:07:19 +0200

    Event Bindings

--- experimental/src/app/app.component.ts
+++ experimental/src/app/app.component.ts
@@ -12,6 +12,8 @@ export class AppComponent {
   obj = {a:1,b:2,c:3}
   // tslint:disable-next-line:max-line-length
   imag = 'https://shwanoff.ru/wp-content/uploads/2018/03/toptal-blog-image-1518187252525-03f6db7b1c131066061024c236c7e3ff-e1521450807573.png';
+
+  inputValue = '';
   // Тут обычняа функция не работает....
   constructor() {
     setTimeout(
@@ -19,6 +21,15 @@ export class AppComponent {
         console.log("Timeout is over");
         this.imag = 'https://cdn.tproger.ru/wp-content/uploads/2018/07/angularMaterial-770x270.jpg';
       }, 5000)
+  }
+
+  onInput(event: KeyboardEvent) {
+    console.log("Event", event);
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  onClick() {
+    console.log("Click!");
   }
 
 

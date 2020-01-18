1cf8cb41b3b80cbac6d822ebeaf78ce7a2de7882
akswift <akswift.freelance@gmail.com>
2020-01-18 17:41:30 +0200

    bindings

--- experimental/src/app/app.component.ts
+++ experimental/src/app/app.component.ts
@@ -9,7 +9,17 @@ export class AppComponent {
   title = 'experimental-title';
   number = 44;
   arr = [1,2,3];
-  obj = {
-    a:1,b:2,c:3
+  obj = {a:1,b:2,c:3}
+  // tslint:disable-next-line:max-line-length
+  imag = 'https://shwanoff.ru/wp-content/uploads/2018/03/toptal-blog-image-1518187252525-03f6db7b1c131066061024c236c7e3ff-e1521450807573.png';
+  // Тут обычняа функция не работает....
+  constructor() {
+    setTimeout(
+      () => {
+        console.log("Timeout is over");
+        this.imag = 'https://cdn.tproger.ru/wp-content/uploads/2018/07/angularMaterial-770x270.jpg';
+      }, 5000)
   }
+
+
 }

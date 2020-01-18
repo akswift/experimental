6c7437fc91d93bdbadca37092912f70f05df7961
akswift <akswift.freelance@gmail.com>
2020-01-18 17:20:11 +0200

    interpolation

--- experimental/src/app/app.component.ts
+++ experimental/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'experimental';
+  title = 'experimental-title';
+  number = 44;
+  arr = [1,2,3];
+  obj = {
+    a:1,b:2,c:3
+  }
 }

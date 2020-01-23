13a00d35dd415fa5dd872053c886c1bef690a613
akswift <akswift.freelance@gmail.com>
2020-01-23 14:04:41 +0200

    ngIFelse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {PostComponent} from './post/post.component';
-import { Post2Component } from './post2/post2.component';
-import { Post3Component } from './post3/post3.component';
-import { Post4Component } from './post4/post4.component';
 import {FormsModule} from "@angular/forms";
 import { ComponentFirstComponent } from './component-first/component-first.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
@@ -14,21 +10,19 @@ import { EventbindingsComponent } from './eventbindings/eventbindings.component'
 import { ToWayBindingsComponent } from './to-way-bindings/to-way-bindings.component';
 import { NgstyleDirectiveComponent } from './ngstyle-directive/ngstyle-directive.component';
 import { NgclassDirectiveComponent } from './ngclass-directive/ngclass-directive.component';
+import { NgifelseComponent } from './ngifelse/ngifelse.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    Post2Component,
-    Post3Component,
-    Post4Component,
     ComponentFirstComponent,
     InterpolationComponent,
     BindingsComponent,
     EventbindingsComponent,
     ToWayBindingsComponent,
     NgstyleDirectiveComponent,
-    NgclassDirectiveComponent
+    NgclassDirectiveComponent,
+    NgifelseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ngifelse/ngifelse.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngifelse',
+  templateUrl: './ngifelse.component.html',
+  styleUrls: ['./ngifelse.component.css']
+})
+export class NgifelseComponent {
+  toggle = true;
+
+}
